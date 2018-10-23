396dabefc70f8e4d5d5bcaca16cfba6a39dd739e
MERIJN <demerijnkerstenatgmail.com>
2018-10-23 17:28:39 +0200

    bugfix

--- src/MyProject.ino
+++ src/MyProject.ino
@@ -11,13 +11,13 @@
  int bitrate[] = {300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600}; //bits per seconde, 9600 is standaard voor photon
 
  GoogleMapsDeviceLocator locator;
- //Thuiskomst thuiskomst;
- //Vertrek vertrek;
+ Thuiskomst thuiskomst;
+ Vertrek vertrek;
 
  void setup()
  {
-     pinMode(/*thuiskomst.GetLed()*/7, OUTPUT);
-     pinMode(/*thuiskomst.GetTempSensor()*/0, OUTPUT);
+     pinMode(thuiskomst.GetLed(), OUTPUT);
+     pinMode(thuiskomst.GetTempSensor(), OUTPUT);
  	   Serial.begin(bitrate[5]);
  	   Particle.function("arrived", atArrived);
  	   locator.withEventName("deviceLocator");
@@ -33,16 +33,16 @@
  {
       if(enterOrExit == "entered")
       {
-        //thuiskomst.KomThuis();
+        thuiskomst.KomThuis();
         blink(4);
-        Particle.publish("postToCalender", "Thuisgekomen"/*calenderMessage.c_str ()*/);
-        Particle.publish("runProgram", "programOff1"/*programToStop.c_str ()*/);
-        Particle.publish("notify", "Welkom thuis"/*notificationMessage.c_str ()*/);
+        Particle.publish("postToCalender", calenderMessage.c_str ());
+        Particle.publish("runProgram", programToStop.c_str ());
+        Particle.publish("notify", notificationMessage.c_str ());
       }
       else
       {
-          //vertrek.Vertrokken();
-          Particle.publish("runProgram", "program1"/*programToStart.c_str ()*/);
+          vertrek.Vertrokken();
+          Particle.publish("runProgram", programToStart.c_str ());
       }
  }
 
@@ -50,9 +50,9 @@
  {
      for(int i = 0; i < numBlinks; i++)
      {
-         digitalWrite(/*Thuiskomst::GetLed()*/7, HIGH);
+         digitalWrite(Thuiskomst::GetLed(), HIGH);
          delay(1000);
-         digitalWrite(/*Thuiskomst::GetLed()*/7, LOW);
+         digitalWrite(Thuiskomst::GetLed(), LOW);
          delay(2000);
      };
  }
